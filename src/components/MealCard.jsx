838e7246feac0faf1a14e84a4a74f426544fa8b6
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-16 00:11:44 +0600

    Meal card updated.

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import StarRatings from "react-star-ratings";
 
 const MealCard = ({ data }) => {
     return (
@@ -12,9 +13,18 @@ const MealCard = ({ data }) => {
             </figure>
             <div className="card-body text-left">
                 <h2 className="card-title text-2xl">{data.title}</h2>
-                <p className="font-semibold">Rating:{data.rating}</p>
-                <p className="font-semibold">Price:{data.price}$</p>
-                <div className="card-actions justify-end">
+                <p>{data.description}</p>
+                <StarRatings
+                    rating={data.rating}
+                    numberOfStars={5}
+                    name="rating"
+                    starRatedColor="#5fbf54"
+                    starDimension="20px"
+                />
+                <div className="flex justify-between items-center mt-4">
+                    <h1 className="text-2xl font-bold text-[#5fbf54]">
+                        {data.price}$
+                    </h1>
                     <Link
                         to={`meals/${data._id}`}
                         className="btn bg-[#5fbf54] text-white border-none"
