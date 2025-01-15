a34e8f435c0898f13cc537aff0a18e36c8b9dfa4
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-15 17:02:18 +0600

    Navbar updated.

--- src/shared/Navbar/NavBar.jsx
+++ src/shared/Navbar/NavBar.jsx
@@ -2,9 +2,10 @@ import { IoMdLogIn } from "react-icons/io";
 import logo from "../../assets/logo/logo.png";
 import { NavLink } from "react-router-dom";
 import { IoNotificationsCircleOutline } from "react-icons/io5";
-import { RiHome9Line } from "react-icons/ri";
+import { RiHome9Line, RiMenuFill } from "react-icons/ri";
 import { GiMeal } from "react-icons/gi";
 import { MdOutlineUpcoming } from "react-icons/md";
+import { LuSquareMenu } from "react-icons/lu";
 
 const NavBar = () => {
     const links = (
@@ -33,25 +34,8 @@ const NavBar = () => {
         <div className="navbar bg-base-100 shadow-md sticky top-0 z-[1] flex justify-between items-center p-2">
             <div>
                 <div className="dropdown">
-                    <div
-                        tabIndex={0}
-                        role="button"
-                        className="btn btn-ghost lg:hidden"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16"
-                            />
-                        </svg>
+                    <div tabIndex={0} role="button" className="lg:hidden">
+                        <RiMenuFill className="text-5xl text-[#5fbf54] border-[#5fbf54] border-2 rounded p-1" />
                     </div>
                     <ul
                         tabIndex={0}
@@ -67,7 +51,7 @@ const NavBar = () => {
                             Guardian{" "}
                             <span className="text-[#5fbf54]">Care</span>
                         </a>
-                        <a className="hidden md:block font-semibold text-sm">
+                        <a className="font-semibold text-xs md:text-sm">
                             Caring Like a Guardian, Always
                         </a>
                     </div>
