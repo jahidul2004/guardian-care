5ec59cad5afc4c59330d76a4926ce96066392514
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-15 18:27:39 +0600

    All catrgory showing functionality added.

--- src/pages/Home/MealsBy.jsx
+++ src/pages/Home/MealsBy.jsx
@@ -30,14 +30,32 @@ const MealsBy = () => {
 
                     <TabPanel>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
-                            {
-                                breakfast.map((item) => <MealCard key={item.id} data={item} ></MealCard>)
-                            }
+                            {breakfast.map((item) => (
+                                <MealCard key={item.id} data={item}></MealCard>
+                            ))}
                         </div>
                     </TabPanel>
-                    <TabPanel>Lunch</TabPanel>
-                    <TabPanel>Dinner</TabPanel>
-                    <TabPanel>All Meals</TabPanel>
+                    <TabPanel>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+                            {lunch.map((item) => (
+                                <MealCard key={item.id} data={item}></MealCard>
+                            ))}
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+                            {dinner.map((item) => (
+                                <MealCard key={item.id} data={item}></MealCard>
+                            ))}
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+                            {allMeals.map((item) => (
+                                <MealCard key={item.id} data={item}></MealCard>
+                            ))}
+                        </div>
+                    </TabPanel>
                 </Tabs>
             </div>
         </div>
