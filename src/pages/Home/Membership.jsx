b841971523e533503bf695cb4a55d85ef37cf94a
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-15 20:20:43 +0600

    Membership card updated.

--- src/pages/Home/Membership.jsx
+++ src/pages/Home/Membership.jsx
@@ -10,8 +10,8 @@ const Membership = () => {
             .then((data) => setMembership(data));
     }, []);
     return (
-        <div className="px-5 md:px-10">
-            <h1 className="text-center font-bold text-3xl py-4 mt-10">
+        <div className="p-5 md:p-10">
+            <h1 className="text-center font-bold text-3xl py-4 mb-10">
                 Elevate Your Experience with Membership
             </h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
