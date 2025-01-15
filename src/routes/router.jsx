35135a6b02c3454c2ed2f7be1c7f83a34490b0f3
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-15 20:02:03 +0600

    Meal details page added.

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,17 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
+import MealDetails from "../pages/details/MealDetails";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout></MainLayout>,
-        children:[
+        children: [
             {
                 path: "/",
-                element:<Home></Home>
-            }
-        ]
+                element: <Home></Home>,
+            },
+            {
+                path: "/meal/:id",
+                element: <MealDetails></MealDetails>,
+                loader: async ({ params }) => {
+                    const data = await fetch(
+                        `https://api.sampleapis.com/recipes/meal/${params.id}`
+                    ).then((res) => res.json());
+                    return data;
+                },
+            },
+        ],
     },
 ]);
 
