b279eb4c95856ba6d4b05daea199ccc106e4bd4c
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-15 18:25:53 +0600

    Meal card added.

---
+++ src/components/MealCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+const MealCard = ({ data }) => {
+    return (
+        <div className="card bg-base-100 shadow-xl">
+            <figure>
+                <img
+                    src={data.image}
+                    alt="Shoes"
+                    className="w-full h-[250px] object-cover"
+                />
+            </figure>
+            <div className="card-body text-left">
+                <h2 className="card-title text-2xl">{data.title}</h2>
+                <p className="font-semibold">Rating:{data.rating}</p>
+                <p className="font-semibold">Price:{data.price}$</p>
+                <div className="card-actions justify-end">
+                    <Link
+                        to={`meal/details/${data.id}`}
+                        className="btn bg-[#5fbf54] text-white border-none"
+                    >
+                        View Details
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MealCard;
--- src/pages/Home/MealsBy.jsx
+++ src/pages/Home/MealsBy.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import useMenu from "../../hooks/useMenu";
+import MealCard from "../../components/MealCard";
 
 const MealsBy = () => {
     const [tabIndex, setTabIndex] = useState(0);
@@ -15,26 +16,30 @@ const MealsBy = () => {
 
     return (
         <div className="flex flex-col items-center gap-4 py-4">
-            <div className="w-max nx-auto">
+            <div className="w-[95%] mx-auto text-center">
                 <Tabs
                     defaultIndex={tabIndex}
                     onSelect={(index) => setTabIndex(index)}
                 >
-                    <TabList className="text-[#5fbf54] md:text-xl font-semibold bg-[#5fbf54] bg-opacity-10 p-2 rounded">
+                    <TabList className="text-[#5fbf54] md:text-xl font-semibold bg-[#5fbf54] bg-opacity-10 p-2 rounded mb-10">
                         <Tab>Breakfast</Tab>
                         <Tab>Lunch</Tab>
                         <Tab>Dinner</Tab>
                         <Tab>All Meals</Tab>
                     </TabList>
 
-                    <TabPanel>Breakfast</TabPanel>
+                    <TabPanel>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+                            {
+                                breakfast.map((item) => <MealCard key={item.id} data={item} ></MealCard>)
+                            }
+                        </div>
+                    </TabPanel>
                     <TabPanel>Lunch</TabPanel>
                     <TabPanel>Dinner</TabPanel>
                     <TabPanel>All Meals</TabPanel>
                 </Tabs>
             </div>
-
-            <div></div>
         </div>
     );
 };
