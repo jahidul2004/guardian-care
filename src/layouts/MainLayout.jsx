055b80eecac51991539258a84d8e31f3ca446136
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-15 16:33:10 +0600

    Main Layout added and theme changed.

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+const MainLayout = () => {
+    return (
+        <div>
+            {/* Navbar */}
+            {/* Navbar end */}
+
+            {/* Outlet */}
+            {/* Outlet end */}
+
+            {/* Footer */}
+            {/* Footer end */}
+        </div>
+    );
+};
+
+export default MainLayout;
