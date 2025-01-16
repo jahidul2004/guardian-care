7677f85795ee1456cf5ef73f27a19a9fd9ebb406
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-17 00:05:43 +0600

    Register user functionality updated.

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -3,12 +3,13 @@ import loginLottie from "../../assets/lottie/login.json";
 import axios from "axios";
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext/AuthContext";
+import Swal from "sweetalert2";
 
 const apiKye = import.meta.env.VITE_IMAGEBB_API_KEY;
 const imgApi = `https://api.imgbb.com/1/upload?key=${apiKye}`;
 
 const Register = () => {
-    const { registerUser } = useContext(AuthContext);
+    const { registerUser, updateUser, setUser } = useContext(AuthContext);
     const handleRegister = async (e) => {
         e.preventDefault();
 
@@ -16,11 +17,6 @@ const Register = () => {
         const email = e.target.email.value;
         const password = e.target.password.value;
         const file = e.target.image.files[0];
-
-        if (!file) {
-            console.error("No file selected for upload!");
-            return;
-        }
 
         try {
             const formData = new FormData();
@@ -36,10 +32,36 @@ const Register = () => {
 
             registerUser(email, password)
                 .then((user) => {
-                    console.log("User registered:", user);
+                    updateUser(name, imageUrl)
+                        .then(() => {
+                            console.log("User registered successfully!");
+                        })
+                        .catch((error) => {
+                            console.error("User update failed:", error);
+                        });
+                    setUser(user);
+                    Swal.fire({
+                        title: "Success!",
+                        text: "Registered successfully!",
+                        icon: "success",
+                        confirmButtonText: "Close",
+                        customClass: {
+                            confirmButton:
+                                "btn bg-[#5fbf54] text-white border-none",
+                        },
+                    });
                 })
                 .catch((error) => {
-                    console.error("User registration failed:", error);
+                    Swal.fire({
+                        title: "Oops!",
+                        text: error.message,
+                        icon: "error",
+                        confirmButtonText: "Close",
+                        customClass: {
+                            confirmButton:
+                                "btn btn-error text-white border-none",
+                        },
+                    });
                 });
         } catch (error) {
             console.error("Image upload failed:", error);
@@ -82,6 +104,7 @@ const Register = () => {
                             <span className="label-text">Profile Picture</span>
                         </label>
                         <input
+                            required
                             name="image"
                             type="file"
                             className="file:bg-[#5fbf54] file:border-none file:text-white file-input file-input-bordered text-[#5fbf54] w-full"
