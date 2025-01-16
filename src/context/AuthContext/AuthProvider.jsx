b41d9749d459f65edacba0f70ca33b66dbb758e1
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-16 23:40:23 +0600

    Update user functionality added.

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import auth from "../../firebase/firebase.init";
 import AuthContext from "./AuthContext";
 
@@ -6,8 +6,16 @@ const AuthProvider = ({ children }) => {
     const registerUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     };
+
+    const updateUser = (displayName, photoURL) => {
+        return updateProfile(auth.currentUser, {
+            displayName,
+            photoURL,
+        });
+    };
     const data = {
         registerUser,
+        updateUser,
     };
 
     return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
