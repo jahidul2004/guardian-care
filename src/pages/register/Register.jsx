6a79b25ac7ee2abfb279f4b008013efde88c3cda
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-16 22:56:56 +0600

    Image upload functionality added.

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,21 +1,53 @@
 import Lottie from "lottie-react";
 import loginLottie from "../../assets/lottie/login.json";
-import { FcGoogle } from "react-icons/fc";
+import axios from "axios";
+
+const apiKye = import.meta.env.VITE_IMAGEBB_API_KEY;
+const imgApi = `https://api.imgbb.com/1/upload?key=${apiKye}`;
 
 const Register = () => {
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const file = e.target.image.files[0];
+
+        if (!file) {
+            console.error("No file selected for upload!");
+            return;
+        }
+
+        try {
+            const formData = new FormData();
+            formData.append("image", file);
+
+            const res = await axios.post(imgApi, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+
+            const imageUrl = res.data.data.url;
+        } catch (error) {
+            console.error("Image upload failed:", error);
+        }
+    };
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 p-5 md:p-10">
             <div className="order-2 card bg-base-100 w-full md:w-[70%] shrink-0 shadow-2xl mx-auto">
                 <h1 className="text-center text-2xl md:text-3xl font-bold my-4 text-[#5fbf54]">
                     Register
                 </h1>
-                <form className="card-body">
+                <form onSubmit={handleRegister} className="card-body">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
                         <input
-                            name="text"
+                            name="name"
                             type="text"
                             placeholder="Enter your name"
                             className="input input-bordered"
@@ -39,8 +71,9 @@ const Register = () => {
                             <span className="label-text">Profile Picture</span>
                         </label>
                         <input
+                            name="image"
                             type="file"
-                            className="file-input file-input-bordered text-[#5fbf54] w-full"
+                            className="file:bg-[#5fbf54] file:border-none file:text-white file-input file-input-bordered text-[#5fbf54] w-full"
                         />
                     </div>
 
@@ -55,14 +88,6 @@ const Register = () => {
                             className="input input-bordered"
                             required
                         />
-                        <label className="label">
-                            <a
-                                href="#"
-                                className="label-text-alt link link-hover"
-                            >
-                                Forgot password?
-                            </a>
-                        </label>
                     </div>
 
                     <div className="form-control mt-6">
