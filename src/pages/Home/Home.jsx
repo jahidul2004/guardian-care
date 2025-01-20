f41beba5e8acb50d1fcc906d80b8fbfb8343c3d6
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-20 10:34:53 +0600

    Payment functionality compleated.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,46 @@
+import { useContext, useEffect, useState } from "react";
 import Banner from "../../components/Banner";
 import Facilities from "./Facilities";
 import MealsBy from "./MealsBy";
 import Membership from "./Membership";
+import AuthContext from "../../context/AuthContext/AuthContext";
+import axios from "axios";
 
 const Home = () => {
+    const [givenBadge, setGivenBadge] = useState("");
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/transactions/${user?.email}`)
+            .then((res) => res.json())
+            .then((data) => {
+                const maxAmountObject = data.reduce((max, current) => {
+                    return current.amount > max.amount ? current : max;
+                }, data[0]);
+
+                if (maxAmountObject?.membershipId === "S001") {
+                    setGivenBadge("silver");
+                } else if (maxAmountObject?.membershipId === "G001") {
+                    setGivenBadge("gold");
+                } else if (maxAmountObject?.membershipId === "P001") {
+                    setGivenBadge("platinum");
+                } else {
+                    setGivenBadge("bronze");
+                }
+            });
+    }, [user]);
+
+    axios
+        .patch(`http://localhost:3000/users/badge/${user?.email}`, {
+            badge: givenBadge,
+        })
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
     return (
         <div>
             {/* Banner */}
